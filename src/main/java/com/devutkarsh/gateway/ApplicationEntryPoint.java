96823b4cae5131ad84efe6c12c70dfe5cdd01787
Dev Utkarsh <devutkarsh@ud.local>
2021-06-30 22:03:16 +0530

    fallback and filter

--- src/main/java/com/devutkarsh/gateway/ApplicationEntryPoint.java
+++ src/main/java/com/devutkarsh/gateway/ApplicationEntryPoint.java
@@ -1,8 +1,13 @@
 package com.devutkarsh.gateway;
 
+import com.devutkarsh.gateway.fallback.DefaultServiceFallback;
+import com.devutkarsh.gateway.filter.RequestFilter;
+import com.devutkarsh.gateway.filter.ResponseFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -12,6 +17,20 @@ public class ApplicationEntryPoint {
 		SpringApplication.run(ApplicationEntryPoint.class, args);
 	}
 
+	@Bean
+	RequestFilter getRequestFilterBean(){
+		return new RequestFilter();
+	}
+
+	@Bean
+	ResponseFilter getResponseFilterBean(){
+		return new ResponseFilter();
+	}
+
+	@Bean
+	DefaultServiceFallback getDefaultServiceFallback(){
+		return new DefaultServiceFallback();
+	}
 
 
 }
